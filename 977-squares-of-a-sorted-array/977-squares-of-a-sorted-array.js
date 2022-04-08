d8b6543d03174e3fd53a74bcedaab1d73e5a4107
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-08 15:01:23 +0530

    Time: 119 ms (75.12%), Space: 48.9 MB (44.93%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -3,13 +3,15 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    const ans = [];
-    while (nums.length) {
-        if (Math.abs(nums[nums.length - 1]) > Math.abs(nums[0])) {
-            ans.unshift(nums.pop() ** 2)
-        } else {
-            ans.unshift(nums.shift() ** 2)
-        }
-    }
-    return ans;
+    // const ans = [];
+    // while (nums.length) {
+    //     if (Math.abs(nums[nums.length - 1]) > Math.abs(nums[0])) {
+    //         ans.unshift(nums.pop() ** 2)
+    //     } else {
+    //         ans.unshift(nums.shift() ** 2)
+    //     }
+    // }
+    // return ans;
+    
+    return nums.map(num => num * num).sort((num1, num2) => num1 - num2);
 };
