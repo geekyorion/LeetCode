156ae78cbb09cd012641ba8bc522a74c12f8641f
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-08 14:20:53 +0530

    Time: 89 ms (73.18%), Space: 43.3 MB (27.10%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        if(obj[target - nums[i]] >= 0) {
+            return [obj[target - nums[i]], i];
+        }
+        obj[nums[i]] = i;
+    }
+};
+
+// with simple object
+    // const obj = {};
+    // for (let i = 0; i < nums.length; i++) {
+    //     if(obj[target - nums[i]] >= 0) {
+    //         return [obj[target - nums[i]], i];
+    //     }
+    //     obj[nums[i]] = i;
+    // }
+
+// trying with brute force
+    // for(let i = 0; i < nums.length; i++) {
+    //     for(let j = i+1; j < nums.length; j++) {
+    //         if(nums[i] + nums[j] === target){
+    //             return [i, j];
+    //         }
+    //     }
+    // }
+
+// with map
+//     const map = new Map();
+//     for(let i = 0; i < nums.length; i++) {
+//         const diff = target - nums[i];
+//         if(map.has(diff)) {
+//             return [map.get(diff), i]
+//         }
+//         map.set(nums[i], i);
+//     }
