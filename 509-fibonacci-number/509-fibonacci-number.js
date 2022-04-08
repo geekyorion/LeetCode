cff23ce05411cc14dffd10ca4d0ab1f5485970dc
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-08 20:28:53 +0530

    Time: 86 ms (54.13%), Space: 41.9 MB (64.32%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var fib = function(n) {
+    // this should be faster than the for loop and memoization
     let phi = ((1 + (5) ** (1 / 2)) / 2) ** n;
     let phi_i = ((1 - (5) ** (1 / 2)) / 2) ** n;
     return ~~((phi - phi_i) / (5) ** (1 / 2));
