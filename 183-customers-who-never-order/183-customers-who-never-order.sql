492ad8b353879571f7f92b088c674e9f5af5ad8a
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-08 22:25:13 +0530

    Time: 762 ms (9.77%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,3 +1,14 @@
 # Write your MySQL query statement below
-SELECT name as Customers FROM Customers
-WHERE NOT EXISTS (SELECT customerId FROM Orders WHERE Orders.customerId = Customers.id);
+SELECT
+    name as Customers
+FROM
+    Customers
+WHERE NOT EXISTS
+    (
+        SELECT
+            customerId
+        FROM 
+            Orders
+        WHERE 
+            Orders.customerId = Customers.id
+    );
