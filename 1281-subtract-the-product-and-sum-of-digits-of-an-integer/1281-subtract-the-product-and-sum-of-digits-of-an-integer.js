f87209876c120095dd8252ecd0701187af77234d
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-09 23:12:36 +0530

    Time: 74 ms (63.59%), Space: 42.1 MB (60.77%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -3,6 +3,16 @@
  * @return {number}
  */
 var subtractProductAndSum = function(n) {
-    const digitArray = [...n.toString()];
-    return eval(digitArray.join('*')) - eval(digitArray.join('+'))
+    // const digitArray = [...n.toString()];
+    // return eval(digitArray.join('*')) - eval(digitArray.join('+'));
+    
+    let sum = 0, multiplication = 1;
+    while (n) {
+        let rem = n % 10;
+        sum += rem;
+        multiplication *= rem;
+        n = ~~(n / 10);
+    }
+    
+    return multiplication - sum;
 };
