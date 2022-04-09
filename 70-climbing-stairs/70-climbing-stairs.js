eb61803d1f82e08135aa189ef3056db0120f2b02
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-10 00:10:49 +0530

    Time: 60 ms (90.25%), Space: 41.9 MB (63.89%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,13 +3,31 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    const memo = { 1: 1, 2: 2, 3: 3, 4: 5 };
+    let first = 2, second = 3;
+    if (n < 4) return n;
     
-    const climbAStair = (by, currentSteps) => {
-        if (currentSteps in memo) return memo[currentSteps];
-        memo[currentSteps] = climbAStair(1, currentSteps - 1) + climbAStair(2, currentSteps - 2);
-        return memo[currentSteps];
+    for (let i = 4; i <= n; i++) {
+        let temp = first;
+        first = second;
+        second = temp + first;
     }
+    return second;
     
-    return climbAStair(0, n);
+//     const memo = { 1: 1, 2: 2, 3: 3, 4: 5 };    
+//     const climbAStair = (by, currentSteps) => {
+//         if (currentSteps in memo) return memo[currentSteps];
+//         memo[currentSteps] = climbAStair(1, currentSteps - 1) + climbAStair(2, currentSteps - 2);
+//         return memo[currentSteps];
+//     }
+    
+//     return climbAStair(0, n);
+    
 };
+
+
+// 1 => 1
+// 2 => 2
+// 3 => 3 (2 + 1)
+// 4 => 5 (3 + 2)
+// 5 => 8 (5 + 3)
+// ... it is fib series
