089070adf1db0579da1d470bd1268a9b134f0d7b
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-09 19:33:12 +0530

    Time: 100 ms (79.54%), Space: 47.3 MB (16.13%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -5,9 +5,13 @@
 var moveZeroes = function(nums) {
     let first_pointer = 0;
     let last_pointer = nums.length - 1;
+    
+    // ignoring the last zeroes
     while (nums[last_pointer] === 0 && last_pointer >= first_pointer) {
         last_pointer--;
     }
+    
+    // need not to check where both pointers meets, that is a non zero number
     while(first_pointer < last_pointer) {
         if (nums[first_pointer] === 0) {
             nums.push(...nums.splice(first_pointer, 1));
