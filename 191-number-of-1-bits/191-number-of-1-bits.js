8a137ec0a31a6f48b618915a3b84d80c36706b7f
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-09 21:53:28 +0530

    Time: 109 ms (23.79%), Space: 44.4 MB (6.29%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -3,6 +3,15 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
+    const str = n.toString(2);
+    let count = 0;
+    for (let char of str) {
+        if (char === '1') {
+            count++;
+        }
+    }
+    return count;
+    
     // parse number to the binary number
     return eval(n.toString(2).split('').join('+'));
     
