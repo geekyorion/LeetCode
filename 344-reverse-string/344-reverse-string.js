9068a1f8aa74ea909428e6f6fb136596e36279e1
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-04-10 23:34:54 +0530

    Time: 87 ms (93.83%), Space: 49.2 MB (80.61%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    s.reverse();
+    
+    // let n = s.length;
+    // while(n--) {
+    //     s.splice(s.pop());
+    // }
+};
