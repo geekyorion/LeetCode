4bf87622db034bcdf1e8cf7cb5ce1882fa6b9c68
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-11-27 22:52:03 +0530

    Time: 146 ms (31.55%), Space: 44.7 MB (59.59%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = nums => {
+    if (nums.length === 0) return 0;
+    
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
