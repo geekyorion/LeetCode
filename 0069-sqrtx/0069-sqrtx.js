de2cbafbce8086d5bfb3e1661f86b9d519b320a0
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-11-28 01:48:34 +0530

    Time: 122 ms (57.36%), Space: 44.1 MB (18.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -5,6 +5,15 @@
 const mySqrt = num => {
     if (num <= 1) return num;
 
+    // using newton's approach
+    let mid = num / 2;
+    while ((mid * mid - num) >= 0.1) {
+        mid = mid - ((mid * mid - num) / (2 * mid));
+    }
+    return ~~mid;
+    
+    
+    // using odd numbers
     let ans = 0;
     for (let i = 1; ;i += 2) {
         num = num - i;
