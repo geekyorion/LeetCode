4ad62a27dae510084ce1bf3c139d00bf84c1ad48
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-11-28 00:26:37 +0530

    Time: 53 ms (99.31%), Space: 42.2 MB (38.16%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,6 +3,30 @@
  * @return {number[]}
  */
 const plusOne = digits => {
+    // using loop
+    const n = digits.length;
+    if (digits[n - 1] < 9) {
+        digits[n - 1] += 1;
+        return digits;
+    } else {
+        digits[n - 1] = 0;
+    }
+
+    let carryForward = true;
+    for (let i = n - 2; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
+    }
+    
+    if (carryForward) {
+        digits.unshift(1);
+    }
+    return digits;
+    
     // using BigInt
     return (
         BigInt(digits.join('')) + BigInt(1)
