5c2d525fcb6fc052768988686723c60fec7d8ab3
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-11-28 01:05:00 +0530

    Time: 74 ms (79.95%), Space: 42.2 MB (30.75%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,6 +2,25 @@
  * @param {string} s
  * @return {number}
  */
+const isAlpha = char => {
+    const code = char.charCodeAt();
+    return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+}
+
 const lengthOfLastWord = s => {
+    // using for loop
+    let firstChar;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (!firstChar && isAlpha(s[i])) {
+            firstChar = i;
+            continue;
+        }
+        if (firstChar && s[i] === ' ') {
+            return firstChar - i;
+        }
+    }
+    return firstChar !== undefined ? firstChar + 1 : 0;
+    
+    // using regex
     return s.replace(/^(.*\s+)?([a-z]+)(\s+)?$/gi, (...args) => args[2]).length;
 };
