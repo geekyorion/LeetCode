058f607f3ced75115739e92481249a1aa544c77f
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-12-08 03:34:42 +0530

    Time: 76 ms (78.75%), Space: 41.8 MB (77.58%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -8,6 +8,7 @@ const getRow = rowIndex => {
 
     const ans = [1];
     let j = 1;
+
     for (; j < ~~((rowIndex + 2) / 2); j++) {
         const last = ans[j - 1];
         ans.push((last * (rowIndex - (j - 1)) / j));
