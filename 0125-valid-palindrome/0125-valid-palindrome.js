425d4507d07c956b82908722b01b655f64d0f1dc
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-12-08 02:49:14 +0530

    Time: 134 ms (31.06%), Space: 44.8 MB (64.57%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+const getASCII = char => char?.charCodeAt(0);
+
+const isAlphaNumeric = char => {
+    const code = getASCII(char);
+    return (code >= 97 && code <= 122) || (code >= 65 && code <= 90) || (code >= 48 && code <= 57);
+};
+
+const getCaseInsensitiveCode = char => {
+    const code = getASCII(char);
+    if (code >= 97) return code - 32;
+    return code;
+};
+
+const isPalindrome = s => {
+    let left = 0;
+    let right = s.length - 1;
+    
+    while (left < right) {
+        if (!isAlphaNumeric(s[left])) { left++; continue; }
+        if (!isAlphaNumeric(s[right])) { right--; continue; }
+        
+        if (getCaseInsensitiveCode(s[left]) !== getCaseInsensitiveCode(s[right])) return false;
+        left++;
+        right--;
+    }
+    
+    return true;
+};
