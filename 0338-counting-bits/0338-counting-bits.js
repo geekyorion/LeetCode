fd65dadd89cb5a05bee17c63230a0a732a91a4f7
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2022-12-20 14:10:41 +0530

    Time: 142 ms (63.18%), Space: 49.8 MB (35.66%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -7,6 +7,23 @@ const countBits = n => {
     if (n === 1) return [0, 1];
 
     const ans = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        ans[i] = ans[~~(i / 2)] + i % 2;
+    }
+
+    return ans;
+};
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+const _countBits = n => {
+    if (n === 0) return [0];
+    if (n === 1) return [0, 1];
+
+    const ans = [0, 1];
     let range = 2;
     let tempRange = 2;
     let currentNum = 1;
