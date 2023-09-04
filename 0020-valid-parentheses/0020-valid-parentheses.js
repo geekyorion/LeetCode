486dfa6a273360d7a97cfb5db2fc35e48bea5b94
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2023-09-05 00:07:11 +0530

    Time: 62 ms (40.09%), Space: 42.9 MB (28.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -1,23 +1,23 @@
+const bracketHash = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+};
+
 /**
  * @param {string} s
  * @return {boolean}
  */
 const isValid = str => {
     if (str.length % 2) return false;
-
-    const brackets = {
-        ')': '(',
-        ']': '[',
-        '}': '{'
-    };
     
     const stack = [];
     
     for (let char of str) {
-        if (char === '(' || char === '{' || char === '[') {
-            stack.push(char);
-        } else {
-            if (stack.pop() !== brackets[char]) return false;
+        if (char in bracketHash) {
+            stack.push(bracketHash[char]);
+        } else if (stack.pop() !== char) {
+            return false;
         }
     }
     
