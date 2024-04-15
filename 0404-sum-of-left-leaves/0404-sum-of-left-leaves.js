94993a1bb9bfdfccfea90c67aaea949c36bfb534
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2024-04-16 00:53:19 +0530

    Time: 62 ms (35.44%), Space: 50.7 MB (95.99%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumOfLeftLeaves = (root) => {
+    let answer = 0;
+    const traverse = (node, shouldSum) => {
+        if (!node.left && !node.right && shouldSum) {
+            answer += node.val;            
+        }
+        if (node.left) traverse(node.left, true);
+        if (node.right) traverse(node.right, false);
+    }
+    traverse(root, false);
+    return answer;
+};
