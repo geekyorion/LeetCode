312809f17d9bd16247fdfb92883049676be80242
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2024-04-16 00:45:38 +0530

    Time: 67 ms (32.83%), Space: 52.6 MB (9.08%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.js
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.js
@@ -3,17 +3,17 @@
  * @return {number[]}
  */
 const deckRevealedIncreasing = deck => {
-    const sorted_deck = deck.sort((a, b) => a - b);
-    const n = sorted_deck.length;
+    deck.sort((a, b) => a - b);
+    const n = deck.length;
     const answer = new Array(n);
-    
+
     let filled = 0;
     let current = 0;
     let skip = true;
 
     while (filled < n) {
         if (skip && !answer[current]) {
-            answer[current] = sorted_deck[filled++];
+            answer[current] = deck[filled++];
             skip = false;
         }
         if (!skip && !answer[current]) {
